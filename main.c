0139e037ebd1d875c385ca209c2e54972f2a99d2
William W Wold <wilcoze@gmail.com>
2018-01-13 12:40:56 -0800

    changed main from C++ to C

--- main.c
+++ main.c
@@ -1,3 +1,38 @@
+
+#include <stdio.h>
+
+#include "memswap.h"
+
+int main(int argc, char** argv)
+{
+	// mem %, swap %, mem total, mem used, swap total, swap used
+	
+	unsigned long memPercent, swapPercent, memTotal, memUsed, swapTotal, swapUsed;
+	
+	int error = read_memswap(&memPercent, &swapPercent, &memTotal, &memUsed, &swapTotal, &swapUsed);
+	
+	if (error==-1)
+	{
+		printf("ERROR: failed to get ram info");
+	}
+	else if (0)
+	{
+		printf("memPercent: %d\n", memPercent);
+		printf("swapPercent: %d\n", swapPercent);
+		printf("memTotal: %d\n", memTotal);
+		printf("memUsed: %d\n", memUsed);
+		printf("swapTotal: %d\n", swapTotal);
+		printf("swapUsed: %d\n", swapUsed);
+	}
+	else
+	{
+		printf("%d%%\n", memPercent);
+	}
+	
+	return 0;
+}
+
+/*
 #include <iostream>
 using std::cout;
 using std::endl;
@@ -33,3 +68,4 @@ int main(int argc, char** argv)
 	
 	return 0;
 }
+*/
